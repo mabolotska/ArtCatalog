e502c668fba5806a08070bcac94e0b0e3a23412b
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2024-02-12 12:22:25 +0100

    created a project

---
+++ ArtCatalog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtCatalog
+//
+//  Created by Maryna Bolotska on 12/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
